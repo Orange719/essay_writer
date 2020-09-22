64226fdf1f34effc7dadc9f05a602404bdc9cde7
Orange719 <70481127+Orange719@users.noreply.github.com>
2020-09-22 15:21:13 +0300

    Yess

---
+++ essay writer.py
@@ -0,0 +1,5 @@
+#essay writer
+essay_title = input("Title for your essay: ")
+your_name = input("Input your name: ")
+essay = input("Write the essay here: ")
+print (essay_title+"\n\n"+essay+"\n\n"+"Made by "+your_name)
