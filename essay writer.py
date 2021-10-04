decd82b287a5e410623af63f1fd81650079e8afe
Poki <51330236+PokiDokika@users.noreply.github.com>
2021-10-04 23:55:10 +0300

    Added a simple html export.
    you can add on this later :)
    maybe add a css file or something to make it pretty. But anyways it's more readable than CLI text so enjoy this upgrade

--- essay writer.py
+++ essay writer.py
@@ -3,3 +3,12 @@ essay_title = input("Title for your essay: ")
 your_name = input("Input your name: ")
 essay = input("Write the essay here: ")
 print (essay_title+"\n\n"+essay+"\n\n"+"Made by "+your_name)
+
+s = input("Export as html? (y/n)")
+
+if(s=="y"):
+  f = open("essay.html", "w")
+  f.write('<!DOCTYPE html> <html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"> <title>Document</title> </head> <body>')
+  f.write("<p style='text-align:right'>"+your_name+"</p>")
+  f.write("<br><h1 style='text-align:center'>"+essay_title+"</h1>")
+  f.write("<br><p>"+essay+"</p")
